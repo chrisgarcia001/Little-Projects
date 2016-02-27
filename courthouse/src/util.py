086a17d08f1a9a52b09be87a3cbb1cd6af2f2324
Chris Garcia <chrisgarcia001@gmail.com>
2016-02-27 15:30:27 -0500

    Adding param reader

--- courthouse/src/util.py
+++ courthouse/src/util.py
@@ -1,5 +1,6 @@
 # A list of utility functions.
 
+# Read a single data path (if app info configures this way).
 def read_data_path(app_info_path):
 	file = open(app_info_path, 'r')
 	for line in file:
@@ -18,7 +19,8 @@ def read_params(app_info_path, sep = ':'):
 		if len(parts) > 1 and not(parts[0].startswith('#')):
 			params[parts[0]] = parts[1]
 	return params
-	
+
+# Print a JSON/dict data set to CSV. Attributes = columns, default is all attributes.	
 def to_csv(data_points, filename, columns=None):
 	if columns == None:
 		columns = list(set(reduce(lambda x,y: x + y, map(lambda z: z.keys(), data_points) + [])))

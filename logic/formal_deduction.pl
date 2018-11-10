3ba1196d7d79dfda025db9a788cfba0068825b62
Christopher Garcia (cgarcia) <chrisgarcia001@gmail.com>
2018-11-10 14:23:59 -0500

    Adding functionality

--- logic/formal_deduction.pl
+++ logic/formal_deduction.pl
@@ -1,5 +1,4 @@
 
-
 % ----------------- Core Theorem Prover ------------------
 rewrite(X, Y, Z) :- equiv(X, Y, Z).
 rewrite(X, Y, Z) :- equiv(Y, X, Z).
@@ -20,11 +19,30 @@ subexp_replace(X, Y, [P|Q], [R|Q]) :- is_list(P), subexp_replace(X, Y, P, R).
 replace(X, Y, R) :- rewrite(X, Y, R).
 replace(X, Y, R) :- subexp(X, A), rewrite(A, B, R), subexp_replace(A, B, X, Y).
 
+eqv_seq_to_proof_format([], []).
+eqv_seq_to_proof_format([A,B], [[A,B]]).
+eqv_seq_to_proof_format([A,B|R], [[A,B]|[S]]) :- eqv_seq_to_proof_format(R, S).
+
+eqv_seq_to_proof(A, R, C) :- reverse([R|A], B), eqv_seq_to_proof_format(B, C).
+
+eqv_seq(A, E, [A, R, E], D) :- 
+	D >= 1, not(has_vars(A)), 
+	replace(A, E, R).
+eqv_seq(A, X, [A,R|P], D) :-
+	D >= 2, not(has_vars(A)), F is D - 1,
+	replace(A, E, R), 
+	eqv_seq(E, X, P, F).
+
 %deduce(Premises, Conclusion, Proof, MaxTotalDepth, MaxEquivalenceDepth, Seen).
 deduce(Prem, Conc, Proof, Depth, Eqv) :- deduce(Prem, Conc, Proof, Depth, Eqv, []).
 deduce([], C, [[C, R]], D, E, _) :- D >= 0, E >= 0, rule([], C, R).
 deduce(Prems, C, [[C, premise]], Depth, Eqv, _) :- 
 	Depth >= 0, Eqv >= 0, member(C, Prems).
+deduce(Prems, C, Proof, Depth, Eqv, _) :- 
+	Depth >= 1, member(P, Prems), eqv_seq(P, C, S, Eqv),
+	eqv_seq_to_proof(S, premise, Proof).
+%deduce(Prems, C, [[C, R],[[P, premise]]], Depth, Eqv, _) :- 
+%	Depth >= 1, Eqv >= 0, member(P, Prems), replace(P, C, R).
 deduce(Prems, Conc, [[Conc, Rule]|Rproofs], Depth, Eqv, Seen) :-
 	Depth > 0, Eqv >= 0, E is Depth - 1, 
 	not(member(Conc, Seen)),
